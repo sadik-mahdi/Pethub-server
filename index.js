21ef60db28b871d954cf8fa4f2d7b65feaed9037
sadik-mahdi <sadikmahadi06@gmail.com>
2026-06-21 18:36:09 +0600

    Added fitering feature function

--- index.js
+++ index.js
@@ -34,48 +34,68 @@ async function run() {
     const petCollection = db.collection("pets");
     const requestsCollection = db.collection("requests");
 
-    app.get('/pet', async(req, res)=>{
-      const result = await petCollection.find().toArray();
-      res.json(result);
-    })
-
-  app.get('/pet/:id', async (req, res) => {
-    try {
-      const { id } = req.params;
-      const isValidId = ObjectId.isValid(id);
-
-      const query = {
-        $or: [
-          {_id : id}, 
-          isValidId ? { _id: new ObjectId(id) } : null 
-          ].filter(Boolean) 
+  app.get("/pet", async (req, res) => {
+    const { search, species } = req.query;
+    const query = {};
+    if (search) {
+      query.petName = {
+        $regex: search,
+        $options: "i",
       };
-
-      const result = await petCollection.findOne(query);
-
-      if (!result) {
-        return res.status(404).json(null);
-      }
-
-      res.json(result);
-    } catch (error) {
-      console.error("Backend single pet fetch error:", error);
-      res.status(500).json({ error: "Internal Server Error" });
     }
+    if (species) {
+      query.species = {
+        $in: species.split(","),
+      };
+    }
+    const result = await petCollection.find(query).toArray();
+    res.json(result);
   });
 
-  app.post('/pet', async(req, res)=>{
-    const petData = req.body
-    console.log(petData);
+  app.get("/pet/:id", requireAuth, async (req, res) => {
+    const { id } = req.params;
+    let query;
+    if (ObjectId.isValid(id)) {
+      query = { _id: new ObjectId(id) };
+    } else {
+      return res.status(400).json({ message: "Invalid ID format" });
+    }
+    const pet = await petCollection.findOne(query);
+    if (!pet) {
+      return res.status(404).json({ message: "Pet not found" });
+    }
+    res.json(pet);
+  });
+
+  app.post("/pet", async (req, res) => {
+    const petData = req.body;
+
     const result = await petCollection.insertOne(petData);
+
     res.json(result);
-  })
+  });
+
+  app.get("/request/:userId", async (req, res) => {
+    const { userId } = req.params;
+    const result = await requestsCollection.find({ userId }).toArray();
+    res.json(result);
+  });
 
   app.post('/request', async(req, res) => {
     const requestData = req.body
     const result = await requestsCollection.insertOne(requestData);
     res.json(result);
   })
+
+  app.delete("/request/:requestId", async (req, res) => {
+    const { requestId } = req.params;
+
+    const result = await requestsCollection.deleteOne({
+      _id: new ObjectId(requestId),
+    });
+
+    res.json(result);
+  });
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
